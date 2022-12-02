55f25159a0e432f9c5bf353772e6a93dbf54b75f
C. Augusto Pinto Machado <40689339+cpmachado@users.noreply.github.com>
2022-12-02 15:41:32 +0000

    Day 02 (#3)
    * Template for day 02
    
    * First draft of day 02 a
    
    * puzzle input
    
    * Solution to day 02 b
    
    * Update README

---
+++ day-02/aoc-02-a.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+int
+evaluate(char opponent, char player) {
+	int nO = opponent - 'A';
+	int nP = player - 'X';
+	int shape = nP + 1;
+
+	if (nO == nP) {
+		return shape + 3;
+	}
+	if ((nO + 1) % 3 == nP) {
+		return shape + 6;
+	}
+
+	return shape;
+}
+
+int
+main(void) {
+	char buffer[BUFSIZ];
+	int sum = 0;
+	char opponent, player;
+
+	while (fgets(buffer, BUFSIZ, stdin)) {
+		if (sscanf(buffer, "%c %c", &opponent, &player) == 2) {
+			sum += evaluate(opponent, player);
+		}
+	}
+
+	printf("%d\n", sum);
+	return 0;
+}
---
+++ day-02/aoc-02-b.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int
+evaluate(char opponent, char res) {
+	int nO = opponent - 'A';
+	int nRes = res - 'X';
+	int wp = nRes * 3;
+	return wp + 1 + (nO + (nRes + 2) % 3) % 3;
+}
+
+int
+main(void) {
+	char buffer[BUFSIZ];
+	int sum = 0;
+	char opponent, res;
+
+	while (fgets(buffer, BUFSIZ, stdin)) {
+		if (sscanf(buffer, "%c %c", &opponent, &res) == 2) {
+			sum += evaluate(opponent, res);
+		}
+	}
+
+	printf("%d\n", sum);
+	return 0;
+}
