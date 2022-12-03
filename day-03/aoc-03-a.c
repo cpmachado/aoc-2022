fa4ea838a1b9d392c19cd6176b9df6561b5db654
cpmachado <cpmachado@protonmail.com>
2022-12-03 12:51:54 +0000

    Solutdion to day 03 a

--- day-03/aoc-03-a.c
+++ day-03/aoc-03-a.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int
+evaluate(char *s) {
+	int n = strlen(s) / 2;
+	char *a = s;
+	char *b = a + n;
+	char *ptr;
+	int res;
+
+	while (a != b) {
+		ptr = b;
+		while (*ptr != '\n') {
+			if (*a == *ptr) {
+				if (islower(*a)) {
+					return (*a - 'a') + 1;
+				} else {
+					return res = (*a - 'A') + 27;
+				}
+			}
+			ptr++;
+		}
+		a++;
+	}
+
+	return 0;
+}
 
 int
 main(void) {
-	printf("Advent Of Code ftw!!\n");
+	char rucksack[BUFSIZ];
+	int sum = 0;
+
+	while (fgets(rucksack, BUFSIZ, stdin)) {
+		sum += evaluate(rucksack);
+	}
+
+	printf("%d\n", sum);
 	return 0;
 }
